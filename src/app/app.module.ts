e211bf7e826a9f4fcc7b154da96929f1a6c85f97
heja1600 <herjan02@gmail.com>
2019-02-22 11:34:31 +0100

    hej

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { MainComponent } from './main/main.component';
+import { MatCardModule } from '@angular/material';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
+    MatCardModule,  
     BrowserModule
   ],
   providers: [],
